d7b8af2c7f2a2fc4389c1cac3ff4edacdf03a8f9
Zach Bartholomew <zach.bartholomew@gmail.com>
2017-09-26 13:51:03 -0700

    update

---
+++ spring-security-and-angular-js/src/main/java/com/zbartholomew/springsecurityandangularjs/SpringSecurityAndAngularJsApplication.java
@@ -0,0 +1,27 @@
+package com.zbartholomew.springsecurityandangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class SpringSecurityAndAngularJsApplication {
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World!");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAndAngularJsApplication.class, args);
+    }
+}
