08d621ae5506979cac18390680316f684ed03d6b
Zach Bartholomew <zach.bartholomew@gmail.com>
2017-09-26 17:54:01 -0700

    Added: - Navigation bar - Login page accessible through nav bar - Authenticate User through login page

--- spring-security-and-angular-js/src/main/java/com/zbartholomew/springsecurityandangularjs/SpringSecurityAndAngularJsApplication.java
+++ spring-security-and-angular-js/src/main/java/com/zbartholomew/springsecurityandangularjs/SpringSecurityAndAngularJsApplication.java
@@ -2,9 +2,16 @@ package com.zbartholomew.springsecurityandangularjs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -13,12 +20,42 @@ import java.util.UUID;
 @RestController
 public class SpringSecurityAndAngularJsApplication {
 
+    // Useful trick in a Spring Security application.
+    // If the "/user" resource is reachable then it will return the
+    // currently authenticated user (an Authentication),
+    // and otherwise Spring Security will intercept the request and
+    // send a 401 response through an AuthenticationEntryPoint
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
     @RequestMapping("/resource")
     public Map<String, Object> home() {
         Map<String, Object> model = new HashMap<>();
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World!");
         return model;
+    }
+
+    // Allowing anonymous access to the static (HTML) resources
+    // (the CSS and JS resources are already accessible by default).
+    // The HTML resources need to be available to anonymous users
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        }
     }
 
     public static void main(String[] args) {
--- spring-security-and-angular-js/src/main/resources/static/js/hello.js
+++ spring-security-and-angular-js/src/main/resources/static/js/hello.js
@@ -1,9 +1,73 @@
-angular.module('hello', [])
-    .controller('home', function ($http) {
-        var self = this;
-        $http.get('/resource').then(function(response) {
-            self.greeting = response.data;
-        })
-    });
+angular.module('hello', ['ngRoute']).config(function ($routeProvider, $httpProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        controller: 'home',
+        controllerAs: 'controller'
+    }).when('/login', {
+        templateUrl: 'login.html',
+        controller: 'navigation',
+        controllerAs: 'controller'
+    }).otherwise('/');
 
-    
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+}).controller('navigation', function ($rootScope, $http, $location, $route) {
+
+    const self = this;
+
+    self.tab = function (route) {
+        return $route.current && route === $route.current.controller;
+    };
+
+    const authenticate = function (credentials, callback) {
+        const headers = credentials ? {
+            authorization: "Basic " + btoa(credentials.username + ":" + credentials.password)
+        } : {};
+
+        $http.get('user', {
+            headers: headers
+        }).then(function (response) {
+            if (response.data.name) {
+                $rootScope.authenticated = true;
+            } else {
+                $rootScope.authenticated = false;
+            }
+            callback && callback($rootScope.authenticated);
+        }, function () {
+            $rootScope.authenticated = false;
+            callback && callback(false);
+        });
+    }
+
+    authenticate();
+
+    self.credentials = {};
+    self.login = function () {
+        authenticate(self.credentials, function (authenticated) {
+            if (authenticated) {
+                console.log("Login succeeded")
+                $location.path("/");
+                self.error = false;
+                $rootScope.authenticated = true;
+            } else {
+                console.log("Login failed")
+                $location.path("/login");
+                self.error = true;
+                $rootScope.authenticated = false;
+            }
+        })
+    };
+
+    self.logout = function () {
+        $http.post('logout', {}).finally(function () {
+            $rootScope.authenticated = false;
+            $location.path("/");
+        });
+    }
+
+}).controller('home', function ($http) {
+    const self = this;
+    $http.get('/resource/').then(function (response) {
+        self.greeting = response.data;
+    })
+});
