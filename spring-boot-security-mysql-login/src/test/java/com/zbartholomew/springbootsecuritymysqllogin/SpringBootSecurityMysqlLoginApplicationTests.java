8f8736176689a663cc172e3c644c71b4cfa94624
Zach Bartholomew <zach.bartholomew@gmail.com>
2017-10-02 16:22:02 -0700

    Spring Boot Security with MySQL

---
+++ spring-boot-security-mysql-login/src/test/java/com/zbartholomew/springbootsecuritymysqllogin/SpringBootSecurityMysqlLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zbartholomew.springbootsecuritymysqllogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSecurityMysqlLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
